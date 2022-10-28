5e08ad579e1320fdff26eef5b93e84d36f00e5df
Cecil Aboagye-Nyarko <caboagye@ford.com>
2022-10-28 22:55:31 +0100

    added starter files for the extension

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
+  if (tab.url && tab.url.includes("*/watch")) {
     const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
 
@@ -9,3 +9,9 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     });
   }
 });
+
+/**
+Publishing the extension: https://developer.chrome.com/docs/webstore/publish/
+Understanding permissions: https://developer.chrome.com/docs/extensions/mv3/declare_permissions/
+
+*/
